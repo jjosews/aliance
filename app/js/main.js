875f07cab90099ab2514b2d03662f315b8f13a4f
Josews <jjosews@gmail.com>
2022-06-05 19:31:41 +0500

    added page

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,13 @@
 $(function(){
+  var header = $(".header");
+  var scrollChange = 1;
+  $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
 
+      if (scroll >= scrollChange) {
+          header.addClass('header-scroll');
+      } else {
+          header.removeClass("header-scroll");
+      }
+  });
 });
